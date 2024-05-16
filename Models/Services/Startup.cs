3c19a6657e0e1166a821fee7fb2cac76e9372f8b
Vinicius Cruzeiro <111810869+VinyEditor@users.noreply.github.com>
2024-05-16 00:51:39 -0300

    Create Startup.cs

---
+++ Models/Services/Startup.cs
@@ -0,0 +1,20 @@
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddHttpClient<ChatGPTService>();
+}
+
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+}
